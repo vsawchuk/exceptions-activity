2b4d960b596dcca6252a6f50a7358ca88d7ca06f
Dan Roberts <dan@adadevelopersacademy.org>
2017-03-04 11:39:11 -0800

    Implement BankAccounts wave 1

---
+++ lib/account.rb
@@ -0,0 +1,34 @@
+module Bank
+  class Account
+    attr_reader :id, :balance
+    def initialize(id, balance)
+      if balance < 0
+        raise ArgumentError.new("Initial balance must be positive (got #{balance})")
+      end
+
+      @id = id
+      @balance = balance
+    end
+
+    def withdraw(amount)
+      if amount < 0
+        raise ArgumentError.new("Cannot withdraw a negative amount (got #{amount})")
+      end
+
+      if @balance - amount < 0
+        puts "Cannot withdraw; balance would go negative"
+        return @balance
+      end
+
+      @balance -= amount
+    end
+
+    def deposit(amount)
+      if amount < 0
+        raise ArgumentError.new("Cannot withdraw a negative amount (got #{amount})")
+      end
+
+      @balance += amount
+    end
+  end
+end
